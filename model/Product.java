deb0a2bced55a8ad85c648bf3f29aac2b455c70f
Samiksha Khmakar <96630204+samikshakhamkar@users.noreply.github.com>
2022-01-05 23:47:42 +0530

    Update Product.java

--- model/Product.java
+++ model/Product.java
@@ -13,7 +13,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
-    private Integer product_id;
+    private Integer productId;
 
     @Column(name = "p_name")
     private String name;
